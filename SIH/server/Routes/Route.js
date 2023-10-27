8173f4c7d613b35339520948d9d94a4e3119e60a
AnmolMaurya5 <anmolmaurya866@gmail.com>
2023-10-27 22:14:17 +0530

    SOme changes

--- SIH/server/Routes/Route.js
+++
@@ -1,5 +0,0 @@
-import express ,{Router} from  'express';
-import { getAllPlaces } from '../Controller/PlacesController.js';
-const router = express.Router();
-// router.get('/places',getAllPlaces);
-export default router;
